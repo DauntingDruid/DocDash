36b59d5bbf3dd3a520dfca6dbae26e169793fbfb
Abhishek <abhishekroxtomar@gmail.com>
2023-05-05 18:05:54 +0530

    Brand name change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
       <h1 className="text-3xl font-bold">
-        Welcome to WoundMedic.ai, how can we help you?
+        Welcome to DocDash, how can we help you?
       </h1>
     </main>
   )
